b802e7d1e1c5066e0a04c3edc78013a9ab518e67
QijunHuMary <huqijunmary@gmail.com>
2022-02-02 16:35:30 -0800

    adding a line

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -45,5 +45,7 @@ public class MarkdownParse {
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
+
+        System.out.println("Make some changes (week 5 lab)");
     }
 }
